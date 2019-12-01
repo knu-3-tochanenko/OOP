2ca843fb8a43755b92bcd32d254ec83cfc8948b9
Vladislav <itochanenko@gmail.com>
2019-12-01 22:10:53 +0200

    Split rotation to four threads

--- Task 12/src/main/java/Main.java
+++ Task 12/src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.swing.*;
 
 public class Main {
@@ -16,24 +17,28 @@ public class Main {
     }
 
     public Main() {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-                }
-
-                JFrame frame = new JFrame("Testing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setBackground(Color.BLACK);
-                frame.setLayout(new BorderLayout());
-                frame.add(new TestPane());
-                frame.setSize(610, 250);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
             }
+
+            JFrame frame = new JFrame("Testing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setBackground(Color.BLACK);
+            frame.setLayout(new BorderLayout());
+            frame.add(new TestPane());
+            frame.setSize(610, 250);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
+    }
+
+    private int[] atomicIntegerToInt(AtomicInteger[] arr) {
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            res[i] = arr[i].get();
+        return res;
     }
 
     public class TestPane extends JPanel implements ActionListener {
@@ -41,111 +46,118 @@ public class Main {
         private Polygon triangle1;
         private Polygon triangle2;
 
-        private int[] centresX = new int[]{150, 300, 450};
-        private int[] centresY = new int[]{121, 121, 121};
+        private AtomicInteger[] centresX = new AtomicInteger[]{
+                new AtomicInteger(150),
+                new AtomicInteger(300),
+                new AtomicInteger(450)
+        };
+        private AtomicInteger[] centresY = new AtomicInteger[]{
+                new AtomicInteger(121),
+                new AtomicInteger(121),
+                new AtomicInteger(121)
+        };
 
-        private int[] trianglesY = new int[]{150, 63, 150};
-        private int[] triangle0X = new int[]{100, 150, 200};
-        private int[] triangle1X = new int[]{250, 300, 350};
-        private int[] triangle2X = new int[]{400, 450, 500};
+        private AtomicInteger[] trianglesY = new AtomicInteger[]{
+                new AtomicInteger(150),
+                new AtomicInteger(63),
+                new AtomicInteger(150)
+        };
+        private AtomicInteger[] triangle0X = new AtomicInteger[]{
+                new AtomicInteger(100),
+                new AtomicInteger(150),
+                new AtomicInteger(200)
+        };
+        private AtomicInteger[] triangle1X = new AtomicInteger[]{
+                new AtomicInteger(250),
+                new AtomicInteger(300),
+                new AtomicInteger(350)
+        };
+        private AtomicInteger[] triangle2X = new AtomicInteger[]{
+                new AtomicInteger(400),
+                new AtomicInteger(450),
+                new AtomicInteger(500)
+        };
 
-        private int[] trianglesStartY = new int[]{150, 63, 150};
-        private int[] triangle0StartX = new int[]{100, 150, 200};
-        private int[] triangle1StartX = new int[]{250, 300, 350};
-        private int[] triangle2StartX = new int[]{400, 450, 500};
-
-        private int iteration = 0;
-
-        private int rotateX(int x, int y, int x0, int y0, int angle) {
-            return (int) (x0 + (x - x0) * Math.cos(Math.toRadians(angle)) - (y - y0) * Math.sin(Math.toRadians(angle)));
-        }
-
-        private int rotateY(int x, int y, int x0, int y0, int angle) {
-            return (int) (y0 + (y - y0) * Math.cos(Math.toRadians(angle)) + (x - x0) * Math.sin(Math.toRadians(angle)));
-        }
+        private AtomicInteger[] trianglesStartY = new AtomicInteger[]{
+                new AtomicInteger(150),
+                new AtomicInteger(63),
+                new AtomicInteger(150)
+        };
+        private AtomicInteger[] triangle0StartX = new AtomicInteger[]{
+                new AtomicInteger(100),
+                new AtomicInteger(150),
+                new AtomicInteger(200)
+        };
+        private AtomicInteger[] triangle1StartX = new AtomicInteger[]{
+                new AtomicInteger(250),
+                new AtomicInteger(300),
+                new AtomicInteger(350)
+        };
+        private AtomicInteger[] triangle2StartX = new AtomicInteger[]{
+                new AtomicInteger(400),
+                new AtomicInteger(450),
+                new AtomicInteger(500)
+        };
 
         private void updateCoords() {
-
-            if (iteration == 360) {
-                trianglesY = trianglesStartY.clone();
-                triangle0X = triangle0StartX.clone();
-                triangle1X = triangle1StartX.clone();
-                triangle2X = triangle2StartX.clone();
-                iteration = 0;
-                return;
-            }
-
-            for (int i = 0; i < 3; i++) {
-                triangle0X[i] = rotateX(
-                        triangle0StartX[i],
-                        trianglesStartY[i],
-                        centresX[0],
-                        centresY[0],
-                        iteration
-                );
-                triangle1X[i] = rotateX(
-                        triangle1StartX[i],
-                        trianglesStartY[i],
-                        centresX[1],
-                        centresY[1],
-                        iteration
-                );
-                triangle2X[i] = rotateX(
-                        triangle2StartX[i],
-                        trianglesStartY[i],
-                        centresX[2],
-                        centresY[2],
-                        iteration
-                );
-                trianglesY[i] = rotateY(
-                        triangle0StartX[i],
-                        trianglesStartY[i],
-                        centresX[0],
-                        centresY[0],
-                        iteration
-                );
-            }
-
-            iteration++;
-
             triangle0 = new Polygon(
-                    triangle0X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle0X),
+                    atomicIntegerToInt(trianglesY),
                     3);
 
             triangle1 = new Polygon(
-                    triangle1X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle1X),
+                    atomicIntegerToInt(trianglesY),
                     3
             );
 
             triangle2 = new Polygon(
-                    triangle2X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle2X),
+                    atomicIntegerToInt(trianglesY),
                     3
             );
         }
 
-        Timer timer = new Timer(5, this);
+        Timer timer = new Timer(Config.SPEED, this);
 
         public TestPane() {
             timer.start();
             triangle0 = new Polygon(
-                    triangle0X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle0X),
+                            atomicIntegerToInt(trianglesY),
                     3);
 
             triangle1 = new Polygon(
-                    triangle1X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle1X),
+                            atomicIntegerToInt(trianglesY),
                     3
             );
 
             triangle2 = new Polygon(
-                    triangle2X,
-                    trianglesY,
+                    atomicIntegerToInt(triangle2X),
+                            atomicIntegerToInt(trianglesY),
                     3
             );
+            Thread triangle0Updater = new Thread(
+                    new CoordUpdater(triangle0X, trianglesY,
+                            triangle0StartX, trianglesStartY, centresX[0], centresY[0], true));
+            Thread triangle1Updater = new Thread(
+                    new CoordUpdater(triangle1X, trianglesY,
+                            triangle1StartX, trianglesStartY, centresX[1], centresY[1], true));
+            Thread triangle2Updater = new Thread(
+                    new CoordUpdater(triangle2X, trianglesY,
+                            triangle2StartX, trianglesStartY, centresX[2], centresY[2], true));
+            Thread triangleYUpdater = new Thread(
+                    new CoordUpdater(triangle0X, trianglesY,
+                            triangle0StartX, trianglesStartY, centresX[0], centresY[0], false));
+            triangle0Updater.setPriority(8);
+            triangle0Updater.start();
+            triangle1Updater.setPriority(4);
+            triangle1Updater.start();
+            triangle2Updater.setPriority(1);
+            triangle2Updater.start();
+            triangleYUpdater.setPriority(9);
+            triangleYUpdater.start();
         }
 
         @Override
@@ -169,9 +181,9 @@ public class Main {
 
             g2d.setColor(Color.RED);
 
-            g2d.drawLine(centresX[0], centresY[0], centresX[0], centresY[0]);
-            g2d.drawLine(centresX[1], centresY[1], centresX[1], centresY[1]);
-            g2d.drawLine(centresX[2], centresY[2], centresX[2], centresY[2]);
+            g2d.drawLine(centresX[0].get(), centresY[0].get(), centresX[0].get(), centresY[0].get());
+            g2d.drawLine(centresX[1].get(), centresY[1].get(), centresX[1].get(), centresY[1].get());
+            g2d.drawLine(centresX[2].get(), centresY[2].get(), centresX[2].get(), centresY[2].get());
 
             g2d.dispose();
         }
