8d167b28ed4bb7a327e5f77843036e1474f80ab8
tochanenko <itochanenko@gmail.com>
2019-12-03 18:15:39 +0200

    Removed Task 16, removed jacoco reports

--- Task 16/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Application frame = new Application();
-        frame.setTitle("Moving dots");
-        frame.setSize(500, 500);
-        frame.start();
-    }
-}
--- Task 16/src/main/java/Side.java
+++
@@ -1,3 +0,0 @@
-public enum Side {
-    LEFT, RIGHT
-}
