cf227d7744d1e652604a90a5176428efed0e106a
Vladislav <itochanenko@gmail.com>
2020-06-13 08:03:26 +0300

    Complete Scheduler

---
+++ Sem 6/Tasks/Task 8/src/main/java/com/tochanenko/StopTask.java
@@ -0,0 +1,16 @@
+package com.tochanenko;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Delayed;
+
+class StopTask extends SingleTask<Void> {
+
+    StopTask() {
+        super(LocalDateTime.now(), () -> null, 0);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return -1;
+    }
+}
