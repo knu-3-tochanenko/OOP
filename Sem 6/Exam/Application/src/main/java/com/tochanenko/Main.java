c0b5292c627317696da1a79ac37814429d56abff
Vladislav <itochanenko@gmail.com>
2020-06-13 21:20:08 +0300

    Add queries to main method

--- Sem 6/Exam/Application/src/main/java/com/tochanenko/Main.java
+++ Sem 6/Exam/Application/src/main/java/com/tochanenko/Main.java
@@ -9,11 +9,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Find triangle with area as close as possible to: ");
-        float area = scanner.nextFloat();
-
         try {
+            System.out.print("Find triangle with area as close as possible to: ");
+            float area = scanner.nextFloat();
             TriangleDao.getClosestByArea(area);
+
+            System.out.print("Find triangles that have sum of areas as close as possible to: ");
+            area = scanner.nextFloat();
+            TriangleDao.getSumOfAreas(area);
+
+            System.out.print("Find circles that csn fit inside circle with radius of: ");
+            float r = scanner.nextFloat();
+            TriangleDao.getInsideCircle(r);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
